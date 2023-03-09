d7d9ffbbadd4d546355b5f8842a2122bb93fb7bb
unknown <wijayagunturadi@gmail.com>
2023-03-09 16:37:38 +0700

    portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,46 @@
+//hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+});
+
+//navbar fixed
+
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
+
+//dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+//memindahkan posisi toggle sesuai mode
+if (
+  localStorage === 'dark' ||
+  (!('theme' in localStorage) && window.matchMedia('(prefers=color-scheme: dark)').matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
